3e379b2e2c581ac1770da1ecf76e7fe3ab3b604a
artem <uvuv.space@gmail.com>
2025-10-30 20:55:11 +0300

    feat: test css modules

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import styles from "./App.module.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. By the way, this is
-          from github actions.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <div className={styles.card}>CSS Modules are working</div>
     </div>
   );
 }
-
-export default App;
